3eff8df1bc256e0dc4856ce8ba5709520eb5bfd7
Blake Bender <bbender@pivotal.io>
2018-06-27 14:02:46 -0700

    WIP: Figuring out how to display text, index now includes length of each entry
    Co-authored-by: Benjamin Ross <bross@pivotal.io

--- fileManagement.js
+++ fileManagement.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var readline = require('readline');
 var stream = require('stream');
+var linesIndex = { timestamps: [], fileNames: [], fileOffsets: [], lengths: []};
 
 function openFile () {
 
@@ -20,6 +21,9 @@ function indexFile(fileName) {
     var outstream = new stream;
     var rl = readline.createInterface(instream, outstream);
     var position = 0;
+    var lastPos = 0;
+    var date = null;
+    var length = 0;
 
     console.log("Scanning file " + fileName);
     
@@ -28,16 +32,39 @@ function indexFile(fileName) {
       var result = line.match(regex);
       if (!(result === null)) {
         // process line here
-        var date = new Date(result[1]);
+        date = new Date(result[1]);
         console.log("Found timestamp: " + result[1] + ", time is " + date.getTime());
+        linesIndex.timestamps.push(date.getTime());
+        linesIndex.fileNames.push(fileName);
+        linesIndex.fileOffsets.push(position);
+        if (linesIndex.fileOffsets.length > 1){
+          linesIndex.lengths.push(length);
+        }
+        length = 0;
       }
+
       console.log("Position is " + position);
       position += line.length + 1;
+      length += line.length + 1;
     });
     
     rl.on('close', function() {
       console.log("Closed file");
+      linesIndex.lengths.push(length);
     });
+
+    console.log("FOO");
 }
 
-document.querySelector('#fileOpenButton').addEventListener('click', openFile)
+function getIndexForPosition(position) {
+
+}
+
+function displayLines(timestamp) {
+  // scan index to position indicated
+  var index = getIndexForTimestamp(timestamp);
+  // read lines at index from indicated file
+
+}
+
+document.querySelector('#fileOpenButton').addEventListener('click', openFile);
